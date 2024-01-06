265b7315c5b46fa95ce04cb53a546fb1f1057c7d
mac <a12@Shahils-MacBook-Pro.local>
2024-01-06 10:43:26 +0530

    updated

---
+++ auto-complete-box/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import TextBox from './Components/TextBox'
+
+function App() {
+
+  return (
+    <>
+<TextBox/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ auto-complete-box/src/Components/TextBox.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import countryData from '../../resources/countryData.json';
+
+function TextBox() {
+  const [val, setVal] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  const changeInput = (e) => {
+    const inputText = e.target.value;
+    setVal(inputText);
+    setSuggestions(getSuggestions(inputText));
+    setShowSuggestions(true);
+  };
+
+  const handleEscapeKey = (e) => {
+    if (e.key === 'Escape') {
+      setShowSuggestions(false);
+    }
+  };
+
+  const searchButton = () => {
+      setShowSuggestions(true);
+    
+  };
+
+  const getSuggestions = (inputText) => {
+    return countryData.filter((country) =>
+      country.name.toLowerCase().includes(inputText.toLowerCase())
+    );
+  };
+
+  return (
+    <div className="main">
+      <h1 className="head">Search any country</h1>
+      <div className="box">
+        <input type="text" onChange={changeInput} onKeyDown={handleEscapeKey}/>
+        <button className="search" onClick={searchButton}>Search</button>
+      </div>
+
+      {showSuggestions && (
+        <ul className="suggestions">
+          {suggestions.map((country, index) => (
+            <p key={index}>{country.name}</p>
+          ))}
+        </ul>
+      )}
+
+      {/* <h1>{val}</h1> */}
+    </div>
+  );
+}
+
+export default TextBox;
+3
